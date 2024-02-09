314dc723b6b72d9f9b85c03296d8ead8afbb7e50
Oussama Bellamine <156456923+BellamineOussama@users.noreply.github.com>
2024-02-09 05:14:09 +0100

    delete

--- Web Fundamentals/week_2/Day_1/Core/Network/script.js
+++
@@ -1,9 +0,0 @@
-var likes = [9, 12, 9]; 
-// Function to increment likes 
-function incrementLikes(index) {
-    // Increment the likes count in the array
-    likes[index - 1]++;
-    // Return the new likes count
-    var counter = document.querySelector('#counter' + index);
-    counter.textContent = likes[index - 1];
-}
