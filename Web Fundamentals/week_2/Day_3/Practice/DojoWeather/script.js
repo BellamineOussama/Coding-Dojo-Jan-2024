ed3007ca8be69ce353f74bb5179a247e2cd024f6
Oussama Bellamine <oussama.bellaminee@gmail.com>
2024-02-19 13:26:13 +0100

    dojo_weather

---
+++ Web Fundamentals/week_2/Day_3/Practice/DojoWeather/script.js
@@ -0,0 +1,11 @@
+// When a user clicks on a city, alert the message "Loading weather report..."
+var payss = document.querySelector("#pays");
+function weatherReport (){
+    alert("Loading weather report...");
+}
+// When the user accepts the cookie policy, hide the fixed message
+var del= document.querySelector(".cookie");
+function removeCookie(){
+    del.remove();
+}
+// When the user changes the <select> convert the temperatures from Celsius to Fahrenheit
