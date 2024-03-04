4d6ac8fcf915cb6565c96204531c6388390d1f38
OussamaBellamine <oussama.bellaminee@gmail.com>
2024-03-04 08:12:15 +0100

    MySQL_countries

--- Python/Week_2/Day 2/Practice/MySQL_Workbench_setup/MySQL_workbench_setup.sql
+++ Python/Week_2/Day 2/Practice/MySQL_Workbench_setup/MySQL_workbench_setup.sql
@@ -3,10 +3,11 @@ INSERT INTO names (name)
 VALUE ('oussama');
 
 INSERT INTO names (name)
-VALUE ('ahmed'),
-	  ('ala'),
-	  ('wael');
-      
+VALUE 
+	('ahmed'),
+	('ala'),
+	('wael');
+
 UPDATE names 
 SET name = 'amine'
 WHERE id = 2;
